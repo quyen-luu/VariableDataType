21c133dff818c3562a1a153f26cc0ccfa0377686
quyen-luu <luunguyenthaoquyen@gmail.com>
2025-05-13 19:16:50 +0700

    Add content

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        Boolean b = true;
+        char c = 'a';
+        string s = "CodeGym";
+
+        Console.WriteLine("Giá trị của biến i là: " + i);
+        Console.WriteLine("Giá trị của biến f là: " + f);
+        Console.WriteLine("Giá trị của biến d là: " + d);
+        Console.WriteLine("Giá trị của biến b là: " + b);
+        Console.WriteLine("Giá trị của biến c là: " + c);
+        Console.WriteLine("Giá trị của biến s là: " + s);
+    }
+}
